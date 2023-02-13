7eb8058440d7fa0b23df0cc75609c1b26a0b7a5a
abdallah <abdallahantony55.aa@gmail.com>
2023-02-13 16:01:40 +0300

    Add constructor

--- item.rb
+++ item.rb
@@ -1,2 +1,10 @@
 class Item
+  attr_reader id, publish_date, archived
+  attr_accessor genre, author, label
+
+  def intialize(id, publish_date, archived)
+    @id = id
+    @publish_date = publish_date
+    @archived = archived
+  end
 end
