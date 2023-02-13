31c995a0de566ccd758acf9b8ee49ca7c3ac74ab
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-02-13 18:26:09 +0500

    Create main.rb file and add the list

---
+++ main.rb
@@ -0,0 +1,17 @@
+def main 
+    puts "\nWelcome to Catalog of my things!\n"
+    inputs =
+      "\nPlease select an option by entering the number:\n" \
+      "1 - List all books\n" \
+      "2 - List all music albums\n" \
+      "3 - List all games\n" \
+      "4 - List all genres\n" \
+      "5 - List all labels\n" \
+      "6 - List all authors\n" \
+      "7 - Add a book\n" \
+      "8 - Add a music album\n" \
+      "9 - Add a game\n" \
+      "10 - Exit\n"
+end
+
+main
