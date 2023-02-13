741ada9d556b69a2f2497fd23ff10780d0259c1f
keebot-png <kee.bot@icloud.com>
2023-02-13 15:12:43 +0200

    Add methods and fix linter errors

--- item.rb
+++ item.rb
@@ -1,10 +1,22 @@
 class Item
-  attr_reader id, publish_date, archived
-  attr_accessor genre, author, label
+  attr_reader :id, :publish_date, :archived
+  attr_accessor :genre, :author, :label
 
-  def intialize(id, publish_date, archived)
+  def intialize(id, publish_date, archived: false)
     @id = id
     @publish_date = publish_date
     @archived = archived
+  end
+
+  private
+
+  def can_be_archived?
+    @publish_date > 10
+  end
+
+  public
+
+  def move_to_archive?
+    @archived = true if can_be_archived?
   end
 end
