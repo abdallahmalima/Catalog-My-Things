1b7fc7b55214f5762f529d17abffdba7064739e7
keebot-png <kee.bot@icloud.com>
2023-02-14 19:58:37 +0200

    Correct linter errors

--- game.rb
+++ game.rb
@@ -1,17 +1,17 @@
-require_relative "item"
+require_relative 'item'
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at
 
-    def initialize(id, publish_date, archived, multiplayer, last_played_at)
-        super(id, publish_date, archived: archived)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  def initialize(id, publish_date, archived, multiplayer, last_played_at)
+    super(id, publish_date, archived: archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    private
+  private
 
-    def can_be_archived?
-        return super && @last_played_at > 2 ? true : false
-    end
+  def can_be_archived?
+    super && @last_played_at > 2
+  end
 end
