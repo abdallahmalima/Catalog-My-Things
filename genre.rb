fb722118e124f67307e42168bf2032b4d1780e34
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-02-14 15:15:50 +0500

    Create Ganre class properties and implement add_item method

--- genre.rb
+++ genre.rb
@@ -1,2 +1,17 @@
 class Genre
+  attr_accessor :id, :name
+  attr_reader :items
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+    @items = []
+
+    private :id, :items
+  end
+
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
 end
