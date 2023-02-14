eb0e7949b03f9959a671915ffc737367c98dc6b3
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-02-14 15:33:31 +0500

    Create all MusicAlbum class properties

--- music_album.rb
+++ music_album.rb
@@ -1,2 +1,13 @@
-class MusicAlbum
+require('./item')
+class MusicAlbum < Item
+  def intialize(id, publish_date, archived: false, on_spotify: false)
+    super(id, publish_date, archived: archived)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && @on_spotify
+  end
+
+  private :can_be_archived?
 end
