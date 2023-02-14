a1ca88c72aff4b6510c2d99ce93acf8c9cc04342
abdallah <abdallahantony55.aa@gmail.com>
2023-02-14 17:56:00 +0300

    Add-book-and-label

---
+++ book.rb
@@ -0,0 +1,19 @@
+require('./item')
+
+class Book < Item
+  attr_reader :publisher, :cover_state
+
+  def initialize(id, publish_date, archived, publisher, cover_state)
+    super(id, publish_date, archived: archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
+end
+
+Book.new(1, 14, true, '2021', 'good')
