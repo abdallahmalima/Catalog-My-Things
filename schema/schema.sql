708357dda1ca9e163b388125628f40eb9cc7568c
abdallah <abdallahantony55.aa@gmail.com>
2023-02-16 18:06:44 +0300

    Add schema for books and labels

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE Genre (
+CREATE TABLE genres (
     id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL
 )
 
-CREATE TABLE MusicAlbum (
+CREATE TABLE music_albums (
     id SERIAL PRIMARY KEY,
 	publish_date DATE NOT NULL,
 	archived BOOLEAN NOT NULL,
@@ -11,17 +11,32 @@ CREATE TABLE MusicAlbum (
 	genre_ID INT REFERENCES Genre(ID)
 )
 
-CREATE TABLE Author (
+CREATE TABLE authors (
     id SERIAL PRIMARY KEY,
 	first_name VARCHAR(100) NOT NULL
 	last_name VARCHAR(100) NOT NULL
 )
 
-CREATE TABLE Game (
+CREATE TABLE games (
     id SERIAL PRIMARY KEY,
 	publish_date DATE NOT NULL,
 	archived BOOLEAN NOT NULL,
 	multiplayer BOOLEAN NOT NULL,
 	last_played DATE NOT NULL
 	author_id INT REFERENCES Author(ID)
+)
+
+CREATE TABLE labels (
+    id SERIAL PRIMARY KEY,
+	title VARCHAR(100) NOT NULL
+	color VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+	publish_date DATE NOT NULL,
+	archived BOOLEAN NOT NULL,
+	publisher VARCHAR(100) NOT NULL,
+	cover_state VARCHAR(100) NOT NULL,
+	label_id INT REFERENCES labels(ID)
 )
