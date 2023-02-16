fb29892847e188191c1dfc6b5b69fb6a575298f0
keebot-png <kee.bot@icloud.com>
2023-02-16 16:58:43 +0200

    Add schema for author and game class

--- schema/schema.sql
+++ schema/schema.sql
@@ -10,3 +10,18 @@ CREATE TABLE MusicAlbum (
 	on_spotify BOOLEAN NOT NULL,
 	genre_ID INT REFERENCES Genre(ID)
 )
+
+CREATE TABLE Author (
+    id SERIAL PRIMARY KEY,
+	first_name VARCHAR(100) NOT NULL
+	last_name VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE Game (
+    id SERIAL PRIMARY KEY,
+	publish_date DATE NOT NULL,
+	archived BOOLEAN NOT NULL,
+	multiplayer BOOLEAN NOT NULL,
+	last_played DATE NOT NULL
+	author_id INT REFERENCES Author(ID)
+)
